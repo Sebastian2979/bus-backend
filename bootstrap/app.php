b173c5257e6f43b693f1a73beb7a64f05d2bf726
Sebastian <sebastian@pumo.digital>
2026-06-15 18:11:07 +0200

    fix schedule gtfs check

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Console\Scheduling\Schedule;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,12 +10,6 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__ . '/../routes/api.php',
         health: '/up',
     )
-    ->withSchedule(function (Schedule $schedule) {
-        $schedule->command('gtfs:check-and-import')
-            ->twiceWeekly(3, 5, '13:00') // Mittwoch & Freitag 13:00
-            ->withoutOverlapping()
-            ->onOneServer();
-    })
     ->withCommands([
         __DIR__ . '/../app/Console/Commands',
     ])
--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+
+Schedule::command('gtfs:check-and-import')
+    ->twiceWeekly(3, 5, '13:00')
+    ->withoutOverlapping()
+    ->onOneServer();
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
